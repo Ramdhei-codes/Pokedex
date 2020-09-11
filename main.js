55c8283d0ca6cbd15100139f7358bee09fcbe65a
Ramdhei <ramdhei2002@gmail.com>
2020-09-11 08:16:58 -0500

    Changed hardcoded number for pokemon name in init function

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ async function getPokemon(id){
 }
 
 async function init(){
-    const firstPokemon = await getPokemon(150);
+    const firstPokemon = await getPokemon('mewtwo');
     upDatePokemon(firstPokemon)
 
 }
