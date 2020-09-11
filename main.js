f655918c369ced836ffcce52608283491e88e85a
Ramdhei <ramdhei2002@gmail.com>
2020-09-11 07:57:55 -0500

    migrated code to a js file

---
+++ main.js
@@ -0,0 +1,28 @@
+async function getPokemon(id){
+    try{
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const data = response.json();
+        return data;  
+    } catch(error){
+        
+        console.error("Pokemon no encontrado")
+    }
+    
+}
+
+async function init(){
+    const firstPokemon = await getPokemon(150);
+    upDatePokemon(firstPokemon)
+
+}
+init()
+
+function upDatePokemon(pokemon){
+    window.pokemon.textContent = pokemon.name
+    window.pokemonImg.src = pokemon.sprites.front_default
+}
+
+window.searchPokemon.addEventListener('change', async() => {
+    const pokemon = await getPokemon(window.searchPokemon.value)
+    upDatePokemon(pokemon)
+})
