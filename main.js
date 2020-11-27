0d1deffd26ac6c9d31a8e00a02bf0139ba34d69e
Ramdhei <ramdhei2002@gmail.com>
2020-11-27 18:05:18 -0500

    Added error 404

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
-async function getPokemon(id){
-    try{
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-        const data = response.json();
-        return data;  
-    } catch(error){
-        
-        console.error("Pokemon no encontrado")
-    }
-    
+async function getPokemon(id) {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const data = response.json();
+    return data;
 }
 
-async function init(){
+function pokemonNotFound() {
+    window.pokemon.textContent = 'Not found'
+    window.pokemonImg.src = './notfound.jpeg'
+    window.pokemonImg.width = 300
+}
+
+async function init() {
     const firstPokemon = await getPokemon('mewtwo');
     upDatePokemon(firstPokemon)
 
 }
 init()
 
-function upDatePokemon(pokemon){
+function upDatePokemon(pokemon) {
     window.pokemon.textContent = pokemon.name
     window.pokemonImg.src = pokemon.sprites.front_default
 }
 
-window.searchPokemon.addEventListener('change', async() => {
-    const pokemon = await getPokemon(window.searchPokemon.value)
-    upDatePokemon(pokemon)
+window.searchPokemon.addEventListener('change', async () => {
+    try {
+        const pokemon = await getPokemon(window.searchPokemon.value.toLowerCase())
+        upDatePokemon(pokemon)
+    } catch (error) {
+        pokemonNotFound()
+    }
+
 })
